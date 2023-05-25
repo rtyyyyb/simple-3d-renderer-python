91d06b8d223846b78465c94cf982c3b2906f6c1c
rtyyyyb <109173130+rtyyyyb@users.noreply.github.com>
2023-05-25 16:13:49 +0100

    Add files via upload

--- renderer.py
+++ renderer.py
@@ -1,18 +1,21 @@
 import turtle as t
+import math as m
+import time
+import numpy
 t.speed(0)
 t.ht()
+t.tracer(0,0)
 focal = 200
-t.tracer(0, 0)
 points = [  
     [
-    [100,100,100],
-    [100,100,200],
-    [200,100,200],
-    [200,100,100],
-    [100,200,100],
-    [100,200,200],
-    [200,200,200],
-    [200,200,100]
+    [-50,-50,-50],
+    [-50,-50,50],
+    [50,-50,50],
+    [50,-50,-50],
+    [-50,50,-50],
+    [-50,50,50],
+    [50,50,50],
+    [50,50,-50]
     ],
     [
     [200,100,100],
@@ -57,18 +60,26 @@ connections = [
     ]
     ]
 
+def clear():
+    t.clear()
+    
 def projectx(x,y,z,focal): #projection matrixes 
     return(x*focal)/(focal+z) 
 def projecty(x,y,z,focal):
     return(y*focal)/(focal+z)
 
+def rotatex(x,y,z,radian): #rotation matrix 
+    t
+    
 def rendershape(xpos,ypos,zpos,yaw,pitch,roll,shape): #object renderer
-    temppoints = points[shape]
+    temppoints = []
     tempconnections = connections[shape]
-    for index in range(len(temppoints)):
-        temppoints[index][0] = temppoints[index][0] + xpos
-        temppoints[index][1] = temppoints[index][1] + ypos
-        temppoints[index][2] = temppoints[index][2] + zpos
+    for index in range(len(points[shape])):
+        tempsubpoints = []
+        tempsubpoints.append(points[shape][index][0] + xpos)
+        tempsubpoints.append(points[shape][index][1] + ypos)
+        tempsubpoints.append(points[shape][index][2] + zpos)
+        temppoints.append(tempsubpoints)
     for line in tempconnections:
         x1 = projectx(temppoints[line[0]-1][0],temppoints[line[0]-1][1],temppoints[line[0]-1][2],focal)
         y1 = projecty(temppoints[line[0]-1][0],temppoints[line[0]-1][1],temppoints[line[0]-1][2],focal)
@@ -78,11 +89,19 @@ def rendershape(xpos,ypos,zpos,yaw,pitch,roll,shape): #object renderer
         t.goto(x1,y1)
         t.pd()
         t.goto(x2,y2)
-    temppoints = []
-    tempconnections = []
-print(points)
-rendershape(-150,-150,-100,0,0,0,0)
-print(points)
-rendershape(-150,-150,-100,0,0,0,0)
-print(points)
+#render code
+for i in range(1000000):
+    posx = math.sin(i/100)*100
+    posy = math.sin((i/100)+1.570795)*100
+    posz = math.sin(i/100)*100
+    clear()
+    rendershape(posx,posy,posz+50,0,0,0,0)
+    posx = math.sin(i/100+3.14159)*100
+    posy = math.sin((i/100+3.14159)+1.570795)*100
+    posz = math.sin(i/100+3.14159)*100
+    rendershape(posx,posy,posz+50,0,0,0,0)
+    t.update()
+    
+
+
 
